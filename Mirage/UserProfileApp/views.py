e47d65460c9123e1b89746b18300990d7f064d87
DTS <officialdts11@gmail.com>
2023-05-11 16:49:08 +0530

    add login view and route

--- Mirage/UserProfileApp/views.py
+++ Mirage/UserProfileApp/views.py
@@ -4,3 +4,6 @@ from django.http import HttpRequest
 # Create your views here.
 def signup(request: HttpRequest):
     return render(request, 'signup.html')
+
+def login(request: HttpRequest):
+    return render(request, 'login.html')
